7365bd602a9819f1cce70a1572610ae577843a9d
Szocs Arnold <arnoldszocs@yahoo.com>
2021-05-28 11:54:52 +0300

    corrected baseURl

--- api/default.service.ts
+++ api/default.service.ts
@@ -31,7 +31,7 @@ import { Configuration }                                     from '../configurat
 })
 export class DefaultService {
 
-    protected basePath = 'http://localhost';
+    protected basePath = 'http://localhost:8080';
     public defaultHeaders = new HttpHeaders();
     public configuration = new Configuration();
     public encoder: HttpParameterCodec;
